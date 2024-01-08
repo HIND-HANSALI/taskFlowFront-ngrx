9b216ea5d8fb4d483e772b6dd702e500f9b3ae0b
HIND-HANSALI <hindhsl111@gmail.com>
2024-01-08 08:28:33 +0100

    structure projet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddTaskComponent } from './components/task/add-task/add-task.component';
+import { AddTagComponent } from './components/tag/add-tag/add-tag.component';
+import { AddUserComponent } from './components/user/add-user/add-user.component';
+import { ListTaskComponent } from './components/task/list-task/list-task.component';
+import { UpdateTaskComponent } from './components/task/update-task/update-task.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTaskComponent,
+    AddTagComponent,
+    AddUserComponent,
+    ListTaskComponent,
+    UpdateTaskComponent,
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
